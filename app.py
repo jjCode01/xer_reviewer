88786a43d44087aa4b3f013a642436ae9fadd8d6
jjCode01 <code@seqmanagement.com>
2021-10-25 21:55:46 -0700

    remove results

--- app.py
+++ app.py
@@ -9,25 +9,10 @@ CODEC = 'cp1252'
 curr_xer = None
 prev_xer = None
 
-@app.route('/')
+@app.route('/', methods=['GET, POST'])
 def index():
-    # if request.method == "POST":
-    #     if (curr := request.files.get('current')) and (prev := request.files.get('previous')):
-    #         curr_xer = Xer(**parse_xer_file(curr.read().decode(CODEC).splitlines()))
-    #         prev_xer = Xer(**parse_xer_file(prev.read().decode(CODEC).splitlines()))
-    #         task_changes = find_task_changes(curr_xer, prev_xer)
-    #         rel_changes = find_relationship_changes(curr_xer, prev_xer)
-    #         res_changes = find_resource_changes(curr_xer, prev_xer)
-        
-    #     print('File Decoded')
-    #     return render_template('results.html', tasks=task_changes, rels=rel_changes, rcrs=res_changes)
-
-    return render_template('index.html')
-
-
-@app.route('/results', methods=['POST'])
-def results():
-    if (curr := request.files.get('current')) and (prev := request.files.get('previous')):
+    if request.method == "POST":
+        if (curr := request.files.get('current')) and (prev := request.files.get('previous')):
             curr_xer = Xer(**parse_xer_file(curr.read().decode(CODEC).splitlines()))
             prev_xer = Xer(**parse_xer_file(prev.read().decode(CODEC).splitlines()))
             find_task_changes(curr_xer, prev_xer)
@@ -35,10 +20,26 @@ def results():
             find_resource_changes(curr_xer, prev_xer)
             projects['current'] = list(curr_xer.projects.values())[0]
             projects['previous'] = list(prev_xer.projects.values())[0]
-        
+    
             return render_template('results.html', changes=changes, projects=projects)
 
     return render_template('index.html')
+
+
+# @app.route('/results', methods=['POST'])
+# def results():
+#     if (curr := request.files.get('current')) and (prev := request.files.get('previous')):
+#         curr_xer = Xer(**parse_xer_file(curr.read().decode(CODEC).splitlines()))
+#         prev_xer = Xer(**parse_xer_file(prev.read().decode(CODEC).splitlines()))
+#         find_task_changes(curr_xer, prev_xer)
+#         find_relationship_changes(curr_xer, prev_xer)
+#         find_resource_changes(curr_xer, prev_xer)
+#         projects['current'] = list(curr_xer.projects.values())[0]
+#         projects['previous'] = list(prev_xer.projects.values())[0]
+    
+#         return render_template('results.html', changes=changes, projects=projects)
+
+#     return render_template('index.html')
 
 
 # if __name__ == '__main__':
